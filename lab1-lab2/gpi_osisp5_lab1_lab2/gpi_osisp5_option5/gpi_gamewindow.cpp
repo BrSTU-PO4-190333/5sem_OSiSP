17503fe0c357b4aee4068eeb3978a0e6dce3e103
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-12-21 13:07:24 +0300

    lab1-lab2/gpi_ : Func restart lvl, func choose lvl

--- lab1-lab2/gpi_osisp5_lab1_lab2/gpi_osisp5_option5/gpi_gamewindow.cpp
+++ lab1-lab2/gpi_osisp5_lab1_lab2/gpi_osisp5_option5/gpi_gamewindow.cpp
@@ -14,16 +14,14 @@ QTimer* gpi_timer;
     connect (gpi_timer, SIGNAL(timeout()), this, SLOT(gpi_animate()));
     gpi_timer->start(100);
 
-    //
-    connect(
-        ui->gpi_action_align,
-        SIGNAL(triggered()),
-        this,
-        SLOT(gpi_on_gpi_action_align_triggered())
-    );
+    connect(ui->gpi_action_align, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_align_triggered()));
+    connect(ui->gpi_action_1_lvl, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_1_lvl_triggered()));
+    connect(ui->gpi_action_2_lvl, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_2_lvl_triggered()));
+    connect(ui->gpi_action_3_lvl, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_3_lvl_triggered()));
+    connect(ui->gpi_action_4_lvl, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_4_lvl_triggered()));
+    connect(ui->gpi_action_restart, SIGNAL(triggered()), this, SLOT(gpi_on_gpi_action_restart_triggered()));
 
-    gpi_generate(gpi_get_1_level());
-    gpi_on_gpi_action_align_triggered();
+    gpi_on_gpi_action_restart_triggered();
 }
 
 gpi_GameWindow::~gpi_GameWindow()
@@ -31,7 +29,7 @@ gpi_GameWindow::~gpi_GameWindow()
     delete ui;
 }
 
-// gpi_ Функция, которая возвращает текстовый первый уровень
+// gpi_ Функция, которая возвращает текстовый 1-ый уровень
 QStringList gpi_get_1_level()
 {
 QStringList gpi_str_map;
@@ -49,8 +47,91 @@ QStringList gpi_str_map;
         "xxxxb.Bbbfxxxxx",
         "xxxx...f..xxxxx",
         "xxxxxxxxxxxxxxx",
+        "xBxxxxxxxxxxxxx",
+        "xBxxxxxxxxxxxxx",
+        "xBxxxxxxxxxxxxx",
+        "xBxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx"
+    };
+    return gpi_str_map;
+}
+
+// gpi_ Функция, которая возвращает текстовый 2-ой уровень
+QStringList gpi_get_2_level()
+{
+QStringList gpi_str_map;
+// = = = = = = = = = = = = = = = =
+    gpi_str_map = {
         "xxxxxxxxxxxxxxx",
         "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxx...xxxxxx",
+        "xxxxf@b..xxxxxx",
+        "xxxxxx.bfxxxxxx",
+        "xxxxfxxb.xxxxxx",
+        "xxxx.x.f.xxxxxx",
+        "xxxxb.Bbbfxxxxx",
+        "xxxx...f..xxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xBxBxxxxxxxxxxx",
+        "xBxBxxxxxxxxxxx",
+        "xBxBxxxxxxxxxxx",
+        "xBxBxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx"
+    };
+    return gpi_str_map;
+}
+
+// gpi_ Функция, которая возвращает текстовый 3-ий уровень
+QStringList gpi_get_3_level()
+{
+QStringList gpi_str_map;
+// = = = = = = = = = = = = = = = =
+    gpi_str_map = {
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxx...xxxxxx",
+        "xxxxf@b..xxxxxx",
+        "xxxxxx.bfxxxxxx",
+        "xxxxfxxb.xxxxxx",
+        "xxxx.x.f.xxxxxx",
+        "xxxxb.Bbbfxxxxx",
+        "xxxx...f..xxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xxxxxxxxxxxxxxx"
+    };
+    return gpi_str_map;
+}
+
+// gpi_ Функция, которая возвращает текстовый 4-ый уровень
+QStringList gpi_get_4_level()
+{
+QStringList gpi_str_map;
+// = = = = = = = = = = = = = = = =
+    gpi_str_map = {
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xxxxxx...xxxxxx",
+        "xxxxf@b..xxxxxx",
+        "xxxxxx.bfxxxxxx",
+        "xxxxfxxb.xxxxxx",
+        "xxxx.x.f.xxxxxx",
+        "xxxxb.Bbbfxxxxx",
+        "xxxx...f..xxxxx",
+        "xxxxxxxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xBxBxBxxxxxxxxx",
+        "xBxxBxxxxxxxxxx",
+        "xBxxBxxxxxxxxxx",
         "xxxxxxxxxxxxxxx"
     };
     return gpi_str_map;
@@ -81,10 +162,10 @@ int gpi_max_width;
     qDebug() << "gpi_mapWidth: " << this->gpi_mapWidth;
 
     // gpi_ Заполняем карту
-    for (gpi_i = 0; gpi_i < this->gpi_mapHeight; ++gpi_i)
+    for (gpi_i = 0; gpi_i < 100; ++gpi_i)
     {
         QVector<gpi_fields> gpi_1d_arr;
-        for (gpi_j = 0; gpi_j < this->gpi_mapWidth; ++gpi_j)
+        for (gpi_j = 0; gpi_j < 100; ++gpi_j)
         {
             gpi_1d_arr.push_back(gpi_err);
         }
@@ -101,21 +182,21 @@ int gpi_max_width;
             // gpi_ Если это пол
             if (gpi_str_map[gpi_i][gpi_j] == '.')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_floor;
+                this->gpi_map[gpi_j][gpi_i] = gpi_floor;
                 continue;
             }
 
             // gpi_ Если это стена
             if (gpi_str_map[gpi_i][gpi_j] == 'x')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_wall;
+                this->gpi_map[gpi_j][gpi_i] = gpi_wall;
                 continue;
             }
 
             // gpi_ Если это игрок
             if (gpi_str_map[gpi_i][gpi_j] == '@')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_player;
+                this->gpi_map[gpi_j][gpi_i] = gpi_player;
                 this->gpi_xPlayer = gpi_i;
                 this->gpi_yPlayer = gpi_j;
                 continue;
@@ -124,14 +205,14 @@ int gpi_max_width;
             // gpi_ Если это финиш
             if (gpi_str_map[gpi_i][gpi_j] == 'f')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_finish;
+                this->gpi_map[gpi_j][gpi_i] = gpi_finish;
                 continue;
             }
 
             // gpi_ Если это игрок на финише
             if (gpi_str_map[gpi_i][gpi_j] == 'A')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_finPlayer;
+                this->gpi_map[gpi_j][gpi_i] = gpi_finPlayer;
                 this->gpi_xPlayer = gpi_i;
                 this->gpi_yPlayer = gpi_j;
                 continue;
@@ -140,18 +221,20 @@ int gpi_max_width;
             // gpi_ Если это коробка
             if (gpi_str_map[gpi_i][gpi_j] == 'b')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_box;
+                this->gpi_map[gpi_j][gpi_i] = gpi_box;
                 continue;
             }
 
             // gpi_ Если это коробка на финише
             if (gpi_str_map[gpi_i][gpi_j] == 'B')
             {
-                this->gpi_map[gpi_i][gpi_j] = gpi_finBox;
+                this->gpi_map[gpi_j][gpi_i] = gpi_finBox;
                 continue;
             }
+            //qDebug() << gpi_j << gpi_map[gpi_j];
         }
         qDebug() << gpi_i << gpi_str_map[gpi_i];
+
     }
 
     qDebug() << this->gpi_map;
@@ -181,9 +264,9 @@ QString gpi_img_path;
     gpi_yk = this->height() / this->gpi_mapHeight;
     gpi_xk = this->width() / this->gpi_mapWidth;
 
-    for (gpi_i = 0; gpi_i < this->gpi_mapHeight; ++ gpi_i)
+    for (gpi_i = 0; gpi_i < this->gpi_mapWidth; ++ gpi_i)
     {
-        for (gpi_j = 0; gpi_j < this->gpi_mapWidth; ++gpi_j)
+        for (gpi_j = 0; gpi_j < this->gpi_mapHeight; ++gpi_j)
         {
             gpi_img_path = get_image_path(this->gpi_map[gpi_i][gpi_j]);
             QPixmap gpi_pixmap(gpi_img_path);
@@ -243,5 +326,68 @@ QString get_image_path(gpi_fields gpi_f)
 // gpi_ Функция для выравнивания окна
 void gpi_GameWindow::gpi_on_gpi_action_align_triggered()
 {
-    this->resize(this->gpi_mapHeight * 32, this->gpi_mapWidth * 32);
+    this->resize(this->gpi_mapWidth * 32, this->gpi_mapHeight * 32);
+}
+
+// gpi_ Функция заполняет массив 1-ым уровнем
+void gpi_GameWindow::gpi_on_gpi_action_1_lvl_triggered()
+{
+    this->gpi_lvl = 1;
+    gpi_on_gpi_action_restart_triggered();
+}
+
+// gpi_ Функция заполняет массив 2-ым уровнем
+void gpi_GameWindow::gpi_on_gpi_action_2_lvl_triggered()
+{
+    this->gpi_lvl = 2;
+    gpi_on_gpi_action_restart_triggered();
+}
+
+// gpi_ Функция заполняет массив 3-им уровнем
+void gpi_GameWindow::gpi_on_gpi_action_3_lvl_triggered()
+{
+    this->gpi_lvl = 3;
+    gpi_on_gpi_action_restart_triggered();
+}
+
+// gpi_ Функция заполняет массив 4-ым уровнем
+void gpi_GameWindow::gpi_on_gpi_action_4_lvl_triggered()
+{
+    this->gpi_lvl = 4;
+    gpi_on_gpi_action_restart_triggered();
+}
+
+// gpi_ Функция, которая рестратит уровень
+void gpi_GameWindow::gpi_on_gpi_action_restart_triggered()
+{
+    if (this->gpi_lvl == 1)
+    {
+        gpi_generate(gpi_get_1_level());
+        gpi_on_gpi_action_align_triggered();
+        return;
+    }
+
+    if (this->gpi_lvl == 2)
+    {
+        gpi_generate(gpi_get_2_level());
+        gpi_on_gpi_action_align_triggered();
+        return;
+    }
+
+    if (this->gpi_lvl == 3)
+    {
+        gpi_generate(gpi_get_3_level());
+        gpi_on_gpi_action_align_triggered();
+        return;
+    }
+
+    if (this->gpi_lvl == 4)
+    {
+        gpi_generate(gpi_get_4_level());
+        gpi_on_gpi_action_align_triggered();
+        return;
+    }
+
+    gpi_generate(gpi_get_1_level());
+    gpi_on_gpi_action_align_triggered();
 }
--- lab1-lab2/gpi_osisp5_lab1_lab2/gpi_osisp5_option5/gpi_gamewindow.hpp
+++ lab1-lab2/gpi_osisp5_lab1_lab2/gpi_osisp5_option5/gpi_gamewindow.hpp
@@ -39,13 +39,22 @@ private:
     int gpi_yPlayer;
     int gpi_mapHeight;
     int gpi_mapWidth;
+    int gpi_lvl;
 
 private slots:
     void gpi_animate();
     void gpi_on_gpi_action_align_triggered();
+    void gpi_on_gpi_action_1_lvl_triggered();
+    void gpi_on_gpi_action_2_lvl_triggered();
+    void gpi_on_gpi_action_3_lvl_triggered();
+    void gpi_on_gpi_action_4_lvl_triggered();
+    void gpi_on_gpi_action_restart_triggered();
 };
 
 QStringList gpi_get_1_level();
+QStringList gpi_get_2_level();
+QStringList gpi_get_3_level();
+QStringList gpi_get_4_level();
 QString get_image_path(gpi_fields gpi_f);
 
 #endif // GPI_GAMEWINDOW_HPP
