cad279cf3004f9bc6c14e14bff9778ee389a9db8
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-12-22 23:20:45 +0300

    lab4/gpi_ : Server public folder

--- lab4/gpi_server/gpi_app.js
+++ lab4/gpi_server/gpi_app.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 app.listen(3002);
 console.log(`Open http://localhost:3002/`);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
     res.send("Hello, world!");
